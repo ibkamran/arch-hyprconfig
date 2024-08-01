e33e7fed6e8cf4ad620a42643e3b0f77b6a230a4
ikmrn <ibragimovkmrn@gmail.com>
2024-08-01 17:40:56 +0500

    feat: Initial commit

---
+++ global_config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##########################
+# Global Configuration   #
+##########################
+
+# This configuration file defines which AUR helpers,
+# code editors, and other tools will be installed.
+# It is sourced by the installation script to determine
+# what to install.
+
+##########################
+# AUR Helpers            #
+##########################
+AUR_HELPERS=("paru" "yay")
+
+##########################
+# Code Editors           #
+##########################
+EDITORS=("vim" "neovim" "visual-studio-code-bin")
+
+##########################
+# Utilities              #
+##########################
+UTILITIES=("git" "firefox" "htop" "neofetch")
